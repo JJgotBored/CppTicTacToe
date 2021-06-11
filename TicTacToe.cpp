43f8cac64633d494699933d30d1a807061047283
JJgotBored <jasonwbere@gmail.com>
2021-06-11 19:33:27 -0400

    - added functionality to medium difficulty game

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <iomanip>
-#include <cstdlib>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 void printBoard(char board[3][3]);
 int mainMenu();
 void onePlayerMenu();
 void easyGame();
+void mediumGame();
 void twoPlayerGame();
 int getNextMove(char board[3][3]);
 char checkWin(char board[3][3]);
 int getEasyMove(char board[3][3]);
+int canWin(char board[3][3]);
+int canLose(char board[3][3]);
+int checkHorizontals (char board[3][3], char search, char other);
+int checkVerticals(char board[3][3],char search, char other);
+int checkDiagonals(char board[3][3],char search, char other);
+
 
 //main
 int main(){
@@ -92,7 +100,7 @@ void onePlayerMenu(){
     if(input == 1)
         easyGame();
     else if(input == 2)
-        cout << "Medium" << endl;
+        mediumGame();
     else if(input == 3)
         cout << "Impossible" << endl;
     else    
@@ -180,7 +188,7 @@ char checkWin(char board[3][3]){
     for(int i = 0; i < 3; i++){
         if (out != ' ')
             return out;
-        else if( (board[0][i] == board[1][i]) && (board[0][i] == board[2][i]) && ((board[0][i] == 'X') || (board[0][1] == 'O')))
+        else if( (board[0][i] == board[1][i]) && (board[0][i] == board[2][i]) && ((board[0][i] == 'X') || (board[0][i] == 'O')))
             out = board[0][i];
     }
     //check left to right diagonal 
@@ -197,13 +205,16 @@ char checkWin(char board[3][3]){
     return out;
 }
 
+//gets a randomised move for the computer
 int getEasyMove(char board[3][3], int turn){
     int move = 0;
     bool valid = false;
+
+    srand(time(NULL));
     move = rand() % (9 - turn);
 
     while (valid == false){
-        move = move % (9 - turn);
+        move = move % 9;
 
         if(board[move/3][move % 3] != ' ')
             move++;
@@ -215,6 +226,7 @@ int getEasyMove(char board[3][3], int turn){
     return move;
 }
 
+//runs game agains easy ai
 void easyGame(){
     char board[3][3];
     int move = 0;
@@ -250,4 +262,169 @@ void easyGame(){
     }
 
     return;
+}
+
+void mediumGame(){
+    char board[3][3];
+    int move = 0;
+    int cWin = -1;
+    int cLose = -1;
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            board[i][j] = ' ';
+        }
+    }
+
+    printBoard(board);
+    for(int i = 0; i < 9; i++){
+        char win = ' ';
+
+        //get move
+        if(i%2 == 0){
+            move = getNextMove(board) -1;
+            board[move/3][move%3] = 'X';
+        }
+        else{
+            cWin = canWin(board);
+            cLose = canLose(board);
+
+            if(cWin >= 0)
+                board[cWin/3][cWin%3] = 'O';
+            else if(cLose >= 0)
+                board[cLose/3][cLose%3] = 'O';
+            else{
+                move = getEasyMove(board, i);
+                board[move/3][move%3] = 'O';
+            }
+
+            cout << "Computer has taken its turn." << endl;
+        }
+        //check for game end
+        win = checkWin(board);
+        if(win != ' '){
+            cout << win << " has won the game." << endl;
+            printBoard(board);
+            return;
+        }
+        printBoard(board);
+    }
+
+    return;
+}
+
+int canWin(char board[3][3]){
+    int move = -1;
+
+    move = checkHorizontals(board, 'O', 'X');
+    if(move >= 0)
+        return move;
+    move = checkVerticals(board, 'O', 'X');
+    if(move >= 0)
+        return move;
+    move = checkDiagonals(board, 'O', 'X');
+    if(move >= 0)
+        return move;
+
+    return -1;
+}
+
+int canLose(char board[3][3]){
+    int move = -1;
+
+    move = checkHorizontals(board, 'X', 'O');
+    if(move >= 0)
+        return move;
+    move = checkVerticals(board, 'X', 'O');
+    if(move >= 0)
+        return move;
+    move = checkDiagonals(board, 'X', 'O');
+    if(move >= 0)
+        return move;
+
+    return -1;
+}
+
+//checks if 3 in a row can occur on horizontal
+int checkHorizontals (char board[3][3], char search, char other){
+    int counter = 0;
+
+    for(int i = 0; i < 3; i++){
+
+        counter = 0;
+        for(int j = 0; j < 3; j++){
+            if(board[i][j] == search)
+                counter++;
+            else if(board[i][j] == other)
+                counter--;
+        }
+
+        if(counter == 2){
+            for(int j = 0; j < 3; j++){
+                if(board[i][j] == ' ')
+                    return (3*i + j);
+            }
+        }
+    }
+
+    return -1;
+}
+
+int checkVerticals(char board[3][3],char search, char other){
+    int counter = 0;
+
+    for(int i = 0; i < 3; i++){
+
+        counter = 0;
+        for(int j = 0; j < 3; j++){
+            if(board[j][i] == search)
+                counter++;
+            else if(board[j][i] == other)
+                counter--;
+        }
+
+        if(counter == 2){
+            for(int j = 0; j < 3; j++){
+                if(board[j][i] == ' ')
+                    return (3*j + i);
+            }
+        }
+    }
+
+    return -1;
+}
+
+int checkDiagonals(char board[3][3],char search, char other){
+    int counter = 0;
+    //left to right diagonal
+    for(int i = 0; i < 3; i++){
+        if(board[i][i] == search)
+            counter++;
+        else if(board[i][i] == other)
+            counter--;
+    }
+
+    if(counter == 2){
+        for(int i = 0; i < 3; i++){
+            if(board[i][i] == ' ')
+                return (3*i + i);
+        }
+    }
+    //right to left diagonal
+    counter = 0;
+    for(int i = 0; i < 3; i++){
+        if(board[2-i][i] == search)
+            counter++;
+        else if(board[2-i][i] == other)
+            counter--;
+    }
+
+    if(counter == 2){
+        for(int i = 0; i < 3; i++){
+            if(board[2-i][i] == ' ')
+                return ( (2-i)*3 +i);
+        }
+    }
+
+    return -1;
 }
